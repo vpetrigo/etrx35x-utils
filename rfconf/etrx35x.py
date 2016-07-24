842c71b857579130055bfa1d4d73b18d17f2fd38
Vladimir Petrigo <taenaru@gmail.com>
2016-07-25 02:06:51 +0300

    add get configuration funcionality

--- rfconf/etrx35x.py
+++ rfconf/etrx35x.py
@@ -8,8 +8,14 @@ class ConfFileError(Exception):
     pass
 
 
+class NodeTypeNotFound(Exception):
+    pass
+
+
 class ModuleConfigurator:
     _XMLFileHeaderTag = "rfconfig"
+    _XMLNodeTag = "node"
+    _XMLNodeTypeAttrName = "type"
 
     def __init__(self, config_file: "XML file with configuration"):
         self.config_file = config_file
@@ -20,10 +26,18 @@ class ModuleConfigurator:
             raise ConfFileError("unsupported XML file format")
 
     def get_node_conf(self, node_type: "node type - COO, FFD, etc"):
-        for node in self.conf_tree.iter("node"):
-            print(node.tag, node.attrib)
-            if node.get("type") == node_type:
-                return list(node)
+        for node in self.conf_tree.iter(self._XMLNodeTag):
+            if node.get(self._XMLNodeTypeAttrName) == node_type:
+                config = []
+                for conf_line in list(node):
+                    config.append(
+                        dict(((tag, val) for tag, val in conf_line.items()),
+                             value=conf_line.text.strip())
+                    )
+                return config
+            raise NodeTypeNotFound(
+                "no such node {} in the XML "
+                "configuration file".format(node_type))
 
     def get_avail_nodes(self):
         available_nodes = self.conf_tree.findall("node")
