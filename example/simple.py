169b381b653262e2db5c4d05b6b2697a49f39523
Vladimir Petrigo <taenaru@gmail.com>
2016-07-26 13:35:45 +0300

    clean up example

--- example/simple.py
+++ example/simple.py
@@ -6,19 +6,6 @@ import serial
 import serial.tools.list_ports
 
 
-def write_conf(device, node_type, config):
-    dev_config = config.get_node_conf(node_type)
-    device.set_node_type(node_type)
-
-    for conf_line in dev_config:
-        new_reg_val = conf_line.value
-        if not conf_line.overwrite:
-            resp = device.register_read(conf_line.reg)
-            new_reg_val = "{:04X}".format(new_reg_val | int(resp[1], 16))
-
-        device.register_write(conf_line.reg, new_reg_val, conf_line.password)
-
-
 def main():
     configurator = rfconf.ModuleConfigReader("conf.xml")
     supp_nodes = configurator.get_avail_nodes()
@@ -32,7 +19,8 @@ def main():
         if com.description.split()[0] == "Telegesis":
             tgmodule = rfconf.ModuleInterface(com.device)
             usr_choice = int(input("How would you like to configure that node: "))
-            write_conf(tgmodule, supp_nodes[usr_choice], configurator)
+            tgmodule.set_node_type(supp_nodes[usr_choice])
+            tgmodule.write_config(configurator)
 
 if __name__ == "__main__":
     main()
