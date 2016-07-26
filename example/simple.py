d44d029481bcf06e2d37fe05590d36e6f84ca901
Vladimir Petrigo <taenaru@gmail.com>
2016-07-26 19:59:57 +0300

    clean up

--- example/simple.py
+++ example/simple.py
@@ -2,25 +2,26 @@
 # coding=utf-8
 
 import rfconf
-import serial
 import serial.tools.list_ports
 
 
 def main():
     configurator = rfconf.ModuleConfigReader("conf.xml")
     supp_nodes = configurator.get_avail_nodes()
-
+    supp_nodes = dict(enumerate(supp_nodes))
+    
     if len(supp_nodes) > 0:
         print("Configuration XML file support {} nodes:".format(len(supp_nodes)))
-        supp_nodes = dict(enumerate(supp_nodes))
         print(supp_nodes)
 
-    for com in serial.tools.list_ports.comports():
-        if com.description.split()[0] == "Telegesis":
-            tgmodule = rfconf.ModuleInterface(com.device)
-            usr_choice = int(input("How would you like to configure that node: "))
-            tgmodule.set_node_type(supp_nodes[usr_choice])
-            tgmodule.write_config(configurator)
+        for com in serial.tools.list_ports.comports():
+            if com.description.split()[0] == "Telegesis":
+                tgmodule = rfconf.ModuleInterface(com.device)
+                usr_choice = int(input("How would you like to configure that node: "))
+                tgmodule.set_node_type(supp_nodes[usr_choice])
+                tgmodule.write_config(configurator)
+    else:
+        print("There is nothing to do")
 
 if __name__ == "__main__":
     main()
