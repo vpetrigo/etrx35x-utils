0ef2e2fe8434eef105c0aecdde36f584182877b1
Vladimir Petrigo <taenaru@gmail.com>
2016-07-28 16:22:37 +0300

    clean up configurator

--- example/simple.py
+++ example/simple.py
@@ -6,21 +6,24 @@ import serial.tools.list_ports
 
 
 def main():
-    configurator = rfconf.ModuleConfigReader("sensor_nwk.xml")
-    supp_nodes = configurator.get_avail_nodes()
-    supp_nodes = dict(enumerate(supp_nodes))
-    
-    if len(supp_nodes) > 0:
-        print("Configuration XML file support {} nodes:".format(len(supp_nodes)))
+    configurator = rfconf.ETRXModuleConfigReader("sensor_nwk.xml")
+    supp_nodes = dict(enumerate(configurator.get_avail_nodes()))
+    node_available = len(supp_nodes)
+    USR_PROMPT = "How would you like to configure that node (-1 - skip): "
+    NODE_FORMAT = "Configuration XML file support {} nodes:"
+
+    if node_available > 0:
+        print(NODE_FORMAT.format(node_available))
         print(supp_nodes)
 
         for com in serial.tools.list_ports.comports():
             if com.description.split()[0] == "Telegesis":
-                tgmodule = rfconf.ModuleInterface(com.device)
+                tgmodule = rfconf.ETRXModule(com.device)
                 print(com.description)
-                usr_choice = int(input("How would you like to configure that node: "))
-                tgmodule.set_node_type(supp_nodes[usr_choice])
-                tgmodule.write_config(configurator)
+                usr_choice = int(input(USR_PROMPT))
+                if usr_choice != -1:
+                    tgmodule.set_node_type(supp_nodes[usr_choice])
+                    tgmodule.write_config(configurator)
     else:
         print("There is nothing to do")
 
