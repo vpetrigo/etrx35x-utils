e10b13cbe47daf212776908fbaa5538c41a69929
Vladimir Petrigo <taenaru@gmail.com>
2016-08-04 16:34:53 +0300

    extend example a little

--- example/bit_access.py
+++ example/bit_access.py
@@ -4,24 +4,29 @@
 import rfconf
 
 
-def get_s0a_bits_value(module):
-    s0a_bit_values = []
+def get_reg_bits_value(module, reg, reg_size):
+    reg_bit_values = []
 
-    for i in range(16):
-        resp = module.register_read_bit("S0A", i)
-        s0a_bit_values.append(int(resp[1]))
+    for i in range(reg_size):
+        resp = module.register_read_bit(reg, i)
+        reg_bit_values.append(int(resp[1]))
 
-    s0a_bit_values.reverse()
+    reg_bit_values.reverse()
 
-    return s0a_bit_values
+    return reg_bit_values
 
 
 def main():
     module = rfconf.ETRXModule("COM22")
     s0a_value = int(module.register_read("S0A")[1], 16)
-    s0a_bits_value = get_s0a_bits_value(module)
+    s0a_bits_value = get_reg_bits_value(module, "S0A", 16)
     converted_value = int("".join(str(x) for x in s0a_bits_value), 2)
     assert converted_value == s0a_value, (converted_value, s0a_value)
+
+    s16_value = int(module.register_read("S16")[1], 16)
+    s16_bits_value = get_reg_bits_value(module, "S16", 32)
+    converted_value = int("".join(str(x) for x in s16_bits_value), 2)
+    assert converted_value == s16_value, (converted_value, s16_value)
 
 
 if __name__ == "__main__":
